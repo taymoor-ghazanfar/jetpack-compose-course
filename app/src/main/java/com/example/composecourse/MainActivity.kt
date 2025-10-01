b4fa2d45447dd3bc13fab7965d0b6970af6cbc92
Taymoor Ghazanfar <taymoor.ghazanfar@venturedive.com>
2025-10-01 20:21:06 +0500

    Added Text, Row, Column, and Button Composables

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecourse.ui.theme.JetpackComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeCourseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(names = NAMES)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeCourseTheme {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String>
+) {
+    Column(modifier = modifier.padding(4.dp)) {
+        names.forEach { name ->
+            Greeting(name)
+        }
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(4.dp, 4.dp)
+    ) {
+        Row(modifier = modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+            ElevatedButton(
+                onClick = {
+
+                }) {
+                Text("Show More")
+            }
+        }
+    }
+}
+
+// Previews
+
+@Preview(
+    name = "Text Preview",
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun MyAppPreview() {
+    JetpackComposeCourseTheme {
+        MyApp(names = NAMES)
+    }
+}
+
+val NAMES = listOf("Taymoor", "Android", "Compose")
