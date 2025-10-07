434d002682f7e8871da7920ad8ac22fb6b92cfb5
Taymoor Ghazanfar <taymoor.ghazanfar@venturedive.com>
2025-10-07 22:12:10 +0500

    Added theme customizations

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composecourse
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,8 +28,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composecourse.ui.theme.Blue
 import com.example.composecourse.ui.theme.JetpackComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -126,14 +131,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = Blue,
+                    contentColor = Color.White
+                ),
                 onClick = {
                     expanded = !expanded
                 }) {
                 Text(
-                    when (expanded) {
+                    text = when (expanded) {
                         true -> "Show Less"
                         false -> "Show More"
                     }
@@ -148,7 +162,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(
     name = "My App Preview",
     showBackground = true,
-    showSystemUi = true
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
 fun MyAppPreview() {
