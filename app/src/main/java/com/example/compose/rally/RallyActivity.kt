2ba304f8adbf146789c9a0cedcc826b5d75b9148
Taymoor Ghazanfar <taymoor.ghazanfar@venturedive.com>
2025-11-12 19:20:26 +0500

    Implemented nav arguments, deep link support, extracted nav host logic into a single file

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,21 +19,16 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.ui.RallyNavHost
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -56,7 +51,8 @@ fun RallyApp() {
         val navController = rememberNavController()
         val currentBackStack by navController.currentBackStackEntryAsState()
         val currentDestination = currentBackStack?.destination
-        val currentScreen=rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Accounts
+        val currentScreen =
+            rallyTabRowScreens.find { it.route == currentDestination?.route } ?: Accounts
         Scaffold(
             topBar = {
                 RallyTabRow(
@@ -68,24 +64,10 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            NavHost(
+            RallyNavHost(
                 navController = navController,
-                startDestination = Overview.route,
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                composable(route = Overview.route) {
-                    Overview.screen()
-                }
-                composable(route = Accounts.route) {
-                    Accounts.screen()
-                }
-                composable(route = Bills.route) {
-                    Bills.screen()
-                }
-            }
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
-            }
+            )
         }
     }
 }
@@ -100,3 +82,7 @@ fun NavHostController.navigateSingleTopTo(route: String) =
         launchSingleTop = true
         restoreState = true
     }
+
+private fun NavHostController.navigateToSingleAccount(accountType: String) {
+    this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+}
