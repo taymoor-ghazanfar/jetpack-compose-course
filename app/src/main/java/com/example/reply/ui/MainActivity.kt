ea75c8d87f9788c99a7651acf1cfaabd5d6b985c
Taymoor Ghazanfar <taymoor.ghazanfar@venturedive.com>
2025-11-28 18:29:49 +0500

    Added material 3 window adaptive info library

--- app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -22,8 +22,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.reply.data.local.LocalEmailsDataProvider
@@ -47,6 +55,17 @@ class MainActivity : ComponentActivity() {
                 ReplyApp(
                     replyHomeUIState = uiState,
                     onEmailClick = viewModel::setSelectedEmail
+                )
+                val adaptiveInfo = currentWindowAdaptiveInfo()
+                val sizeClassText =
+                    "${adaptiveInfo.windowSizeClass.windowWidthSizeClass}\n" +
+                            "${adaptiveInfo.windowSizeClass.windowHeightSizeClass}"
+                Text(
+                    text = sizeClassText,
+                    color = Color.Magenta,
+                    modifier = Modifier.padding(
+                        WindowInsets.safeDrawing.asPaddingValues()
+                    )
                 )
             }
         }
